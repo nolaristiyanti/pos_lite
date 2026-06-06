870216b98dac67d578642897c09f53e5f8302cf2
Nola Ristiyanti <nolaristiyanti.com>
2026-06-06 22:42:32 +0700

    feat(transaction): create transaction tables and relationships

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\TransactionDetail;
 
 class Product extends Model
 {
@@ -19,5 +20,10 @@ class Product extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function transactionDetails()
+    {
+        return $this->hasMany(TransactionDetail::class);
     }
 }
