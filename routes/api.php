9d756a515b4e7a1aca16403501a5e6ef79f70f8c
Nola Ristiyanti <nolaristiyanti.com>
2026-06-06 22:24:13 +0700

    feat(product): register product routes and authorization

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,6 +25,9 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
+});
 
-    Route::middleware('admin')->apiResource('categories',CategoryController::class);
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('products', ProductController::class);
 });
