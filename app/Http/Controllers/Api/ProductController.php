9b333aea52804d684a36c5833584c36f0a8b1f4b
Nola Ristiyanti <nolaristiyanti.com>
2026-06-06 22:13:02 +0700

    feat(product): implement product crud controller

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Product;
+use Illuminate\Http\JsonResponse;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
+
+class ProductController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        $products = Product::with('category')->latest()->get();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Product list retrieved successfully',
+            'data' => $products,
+        ]);
+    }
+
+    public function store(StoreProductRequest $request): JsonResponse
+    {
+        $product = Product::create($request->validated());
+
+        $product->load('category');
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Product created successfully',
+            'data' => $product,
+        ], 201);
+    }
+
+    public function show(Product $product): JsonResponse
+    {
+        $product->load('category');
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Product retrieved successfully',
+            'data' => $product,
+        ]);
+    }
+
+    public function update(
+        UpdateProductRequest $request,
+        Product $product
+    ): JsonResponse {
+        $product->update($request->validated());
+
+        $product->load('category');
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Product updated successfully',
+            'data' => $product,
+        ]);
+    }
+
+    public function destroy(Product $product): JsonResponse
+    {
+        $product->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Product deleted successfully',
+        ]);
+    }
+}
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use App\Models\Product;
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
 
 class Category extends Model
 {
