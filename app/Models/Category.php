b8bf71de7507c5565c8df537996d75470d5a787e
Nola Ristiyanti <nolaristiyanti.com>
2026-06-06 21:54:32 +0700

    feat(product): create product model migration and relationships

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,10 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Product;
 
 class Category extends Model
 {
     protected $fillable = [
         'name',
     ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
